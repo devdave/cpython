004f26ecea72a2951ee72f797e90a565bfd8af92
devdave <devdave@ominian.net>
2022-05-12 15:49:34 -0600

    Issue 92359, 'Edit with IDLE' context menu won't open files names code.py because of a python import path issue where idlelib\pyshell.py has a `from code import...`.

--- Lib/idlelib/__main__.py
+++ Lib/idlelib/__main__.py
@@ -3,6 +3,10 @@ IDLE main entry point
 
 Run IDLE as python -m idlelib
 """
+import sys
+import pathlib
+sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.absolute()))
+
 import idlelib.pyshell
 idlelib.pyshell.main()
 # This file does not work for 2.7; See issue 24212.
